cef4a64b54e757f7016f267867abbc7c87d10c7e
Tushar Kiran <kirantushar10@gmail.com>
2025-12-18 10:17:55 +0100

    Docker CI/CD yaml file

--- test_app.py
+++ test_app.py
@@ -1,1 +1,9 @@
 ## Any File/Folder that begins with 'test' in its name will be considered by pytest.
+
+from app import app
+
+def test_home():
+    response = app.test_client().get('/')
+
+    assert response.status_code == 200
+    assert response.data == b"Hello, World!"
